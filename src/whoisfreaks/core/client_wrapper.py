fd68feb075bfe2a632ebbebf32dc6fac35e3080a
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-05-15 11:09:19 +0000

    Release 0.0.44

--- src/whoisfreaks/core/client_wrapper.py
+++ src/whoisfreaks/core/client_wrapper.py
@@ -13,10 +13,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "wf-python-sdk/0.0.43",
+            "User-Agent": "wf-python-sdk/0.0.44",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "wf-python-sdk",
-            "X-Fern-SDK-Version": "0.0.43",
+            "X-Fern-SDK-Version": "0.0.44",
         }
         return headers
 
